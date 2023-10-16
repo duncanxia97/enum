aa6684efa3ea3433a814218d6584abf682c1a6bf
duncanxia <duncanxia97@163.com>
2023-10-16 13:51:18 +0800

    抽离部分代码

---
+++ .phpstorm.meta.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PHPSTORM_META {
+
+    // Reflect
+    override(\Fatbit\Enums\Traits\GetEnumAttributes::getEnumCaseAttribute(0), map('@'));
+    override(\Fatbit\Enums\Traits\GetEnumAttributes::getEnumAttribute(0), map('@'));
+
+}
---
+++ src/Traits/GetEnumAttributes.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @author XJ.
+ * @Date   2023/9/20 0020
+ */
+
+namespace Fatbit\Enums\Traits;
+
+use ReflectionEnum;
+use ReflectionEnumUnitCase;
+
+trait GetEnumAttributes
+{
+
+    /**
+     * @author XJ.
+     * @Date   2023/9/20 0020
+     * @template T
+     *
+     * @param string|class-string<T> $attribute
+     *
+     * @return array|T[]
+     * @throws \ReflectionException
+     */
+    public function getEnumAttributes(string $attribute): array
+    {
+        return array_map(
+            fn($reflectionAttribute) => $reflectionAttribute->newInstance(),
+            (new ReflectionEnum($this))
+                ->getAttributes($attribute)
+        );
+    }
+
+    /**
+     * @author XJ.
+     * @Date   2023/9/20 0020
+     * @template T
+     *
+     * @param string|class-string<T> $attribute
+     *
+     * @return object|null|T
+     */
+    public function getEnumAttribute(string $attribute, int $get = 0): ?object
+    {
+        return ((new ReflectionEnum($this))
+                    ->getAttributes($attribute)[$get] ?? null)
+            ?->newInstance();
+    }
+
+    /**
+     *
+     * @author XJ.
+     * @Date   2023/9/20 0020
+     * @template T
+     *
+     * @param string|class-string<T> $attribute
+     *
+     * @return array|T[]
+     */
+    public function getEnumCaseAttributes(string $attribute): array
+    {
+        return array_map(
+            fn($reflectionAttribute) => $reflectionAttribute->newInstance(),
+            (new ReflectionEnumUnitCase($this, $this->name))
+                ->getAttributes($attribute)
+        );
+    }
+
+    /**
+     * @author XJ.
+     * @Date   2023/9/20 0020
+     * @template T
+     *
+     * @param string|class-string<T> $attribute
+     *
+     * @return object|null|T
+     */
+    public function getEnumCaseAttribute(string $attribute, int $get = 0): ?object
+    {
+        return ((new ReflectionEnumUnitCase($this, $this->name))
+                    ->getAttributes($attribute)[$get] ?? null)
+            ?->newInstance();
+    }
+}
